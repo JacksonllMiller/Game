410539f9c29c4433ecfa5e112a303c463861c6ff
JacksonMiller <jacksonmiller@mmc.school.nz>
2019-08-22 12:26:53 +1200

    Update

---
+++ PingPongGame/_Classes/Data.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game.Classes
+{
+        static class Data
+        {
+            private const string relativePath = "jacksonmiller/highscores";
+            private static readonly string dataPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments, Environment.SpecialFolderOption.Create),
+                relativePath);
+
+            public static bool Save<T>(T data, string fileName)
+            {
+                FileStream fs = new FileStream(Path.Combine(dataPath, $"{fileName}.jpp"), FileMode.Create);
+
+                BinaryFormatter formatter = new BinaryFormatter();
+                try
+                {
+                    formatter.Serialize(fs, data);
+                }
+                catch (SerializationException e)
+                {
+                    Console.WriteLine("Failed to serialize. Reason: " + e.Message);
+                    return false;
+                }
+
+                fs.Close();
+                return true;
+            }
+
+            public static T Load<T>(string fileName)
+            {
+                FileStream fs = new FileStream("DataFile.dat", FileMode.Open);
+                T data;
+                try
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    data = (T)formatter.Deserialize(fs);
+                }
+                catch (SerializationException e)
+                {
+                    Console.WriteLine("Failed to deserialize. Reason: " + e.Message);
+                    throw;
+                }
+
+                fs.Close();
+                return data;
+            }
+        }
+}
--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-# Game
-301COS Game
-Code found in gioco2/form1.cs
-All planning found at the Wiki, also testing and play through video.
-Highscore saving no longer in code due to an issue that it's progress to be lost. Screenshots can be found on the wiki also aswell as it being in the video.
+# PingPongGame
+ 
